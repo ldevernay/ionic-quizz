36cbafdc6f20d58f78197964cc3c0612bdc902d2
ldevernay <boblehamster@yahoo.fr>
2017-06-19 15:19:44 +0200

    score+message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,6 +18,7 @@ export class HomePage {
   questionForm;
   questions;
   score;
+  message;
 
   constructor(public navCtrl: NavController, public http: Http) {
     this.http.get('https://opentdb.com/api.php?amount=1&category=18&type=boolean').map(res => res.json()).subscribe(data => {
@@ -31,23 +32,26 @@ export class HomePage {
     "answers": new FormControl({value: 'nope', disabled: false})
   });
   this.score = 0;
+  this.message = '';
 }
 
 doSubmit(event) {
   console.log('Submitting form', this.questionForm.value.answers);
   console.log('Answer : ', this.questions[0].correct_answer)
   if (this.questionForm.value.answers == this.questions[0].correct_answer){
-    this.http.get('https://opentdb.com/api.php?amount=1&category=18&type=boolean').map(res => res.json()).subscribe(data => {
-      this.questions = data.results;
-    },
-    err => {
-      console.log("Oops!");
-    }
-  );
   this.score++;
+  this.message = 'Well done!';
   } else {
+    this.message = 'Too bad, your score was : '+this.score;
     this.score = 0;
   }
+  this.http.get('https://opentdb.com/api.php?amount=1&category=18&type=boolean').map(res => res.json()).subscribe(data => {
+    this.questions = data.results;
+  },
+  err => {
+    console.log("Oops!");
+  }
+);
   event.preventDefault();
 }
 
