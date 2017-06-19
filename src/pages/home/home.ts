aa2daac0117aaa6c56439ca8f963a88f7c2c231a
ldevernay <boblehamster@yahoo.fr>
2017-06-19 14:30:30 +0200

    form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import {
+  FormGroup,
+  FormControl
+
+} from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -9,17 +14,51 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  questions: any;
+  langs;
+    langForm;
+    questions;
 
-  constructor(public navCtrl: NavController, public http: Http) {
-
-    this.http.get('https://opentdb.com/api.php?amount=10&category=18&type=boolean').map(res => res.json()).subscribe(data => {
-      this.questions = data.results;
-    },
-    err => {
+    constructor(public navCtrl: NavController, public http: Http) {
+      this.http.get('https://opentdb.com/api.php?amount=1&category=18&type=boolean').map(res => res.json()).subscribe(data => {
+            this.questions = data.results;
+          },
+          err => {
             console.log("Oops!");
-        }
-  );
-  }
+          }
+        );
+      this.langForm = new FormGroup({
+        "langs": new FormControl({value: 'rust', disabled: false})
+      });
+    }
+
+    doSubmit(event) {
+      console.log('Submitting form', this.langForm.value);
+      event.preventDefault();
+    }
+
+//   questions: any;
+//   answers: any;
+//
+//   constructor(public navCtrl: NavController, public http: Http) {
+//
+//     this.http.get('https://opentdb.com/api.php?amount=1&category=18&type=boolean').map(res => res.json()).subscribe(data => {
+//       this.questions = data.results;
+//     },
+//     err => {
+//       console.log("Oops!");
+//     }
+//   );
+// }
+//
+// answer(){
+//   console.log("test");
+//   this.http.get('https://opentdb.com/api.php?amount=1&category=18&type=boolean').map(res => res.json()).subscribe(data => {
+//     this.questions = data.results;
+//   },
+//   err => {
+//     console.log("Oops!");
+//   }
+// );
+// }
 
 }
